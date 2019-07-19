6020bb59f2dbbe793b9e63acf41cf3c76598aa0e
Jamie Bray <jamie@jamiebray.xyz>
2019-07-19 23:55:30 +0800

    Improved dotfiles setup script text

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,6 +1,9 @@
 cd $HOME
 git --git-dir=$HOME/repos/dotfiles/.git --work-tree=$HOME checkout -- .gitignore
 cd -
-echo "Run 'dot status' to determine if any changes will be lost"
-echo "Run 'dot checkout -- .' to reset dotfiles"
+echo
+echo "Please run 'dot status' to determine if any changes will be lost"
+echo
+echo "Followed by 'dot checkout -- .' to reset dotfiles"
+echo
 echo "Then log out and back in again!"
