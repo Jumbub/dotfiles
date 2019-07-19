c96549196cce09082f5f9cafd2efdfa75cc88071
Jamie Bray <jamie@jamiebray.xyz>
2019-07-19 23:54:09 +0800

    Fixed script typo

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,5 +1,5 @@
 cd $HOME
-git --git-dir=$HOME/dotfiles/.git --work-tree=$HOME checkout -- .gitignore
+git --git-dir=$HOME/repos/dotfiles/.git --work-tree=$HOME checkout -- .gitignore
 cd -
 echo "Run 'dot status' to determine if any changes will be lost"
 echo "Run 'dot checkout -- .' to reset dotfiles"
