e85aeec2b89d475b1ddd1433c92fe4ada46fb2e7
Jamie Bray <jamie@jamiebray.xyz>
2019-07-20 02:39:14 +0800

    Improved dotfiles init script

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,5 +1,8 @@
 cd $HOME
 git --git-dir=$HOME/repos/dotfiles/.git --work-tree=$HOME checkout -- .gitignore
 echo "Please run:"
-echo "source ~/repos/dotfiles/.aliases"
+echo "> source ~/repos/dotfiles/.aliases"
+echo
+echo "and then:"
+echo "> dot-checkout"
 cd -
