1957d25a3208532e19e340f044ebf17e2df9f2cb
Jamie Bray <jamie@jamiebray.xyz>
2019-07-21 23:25:24 +0800

    Updated dotfiles script to change dotfiles repo into a bare repo

--- scripts/setup/2-dotfiles.sh
+++ scripts/setup/2-dotfiles.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+
+# Change the default shell
 sudo chsh -s /usr/bin/zsh $USER
+
+# Source alises
 source $HOME/repos/dotfiles/.aliases
+
+# Convert dotfiles repo into a bare repository
+mv $HOME/repos/dotfiles/.git $HOME/repos/dotfiles.git
+rm -rf $HOME/repos/dotfiles
+
+# Run alias to apply changes
 dotcheckout
