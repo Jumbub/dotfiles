b3ce1bd407ff1bc0ca34c420efdde85ea918536e
Jamie Bray <jamie@jamiebray.xyz>
2022-02-04 16:03:59 +0800

    more dirty python tooling

--- .config/nvim/lua/formatter-config.lua
+++ .config/nvim/lua/formatter-config.lua
@@ -8,6 +8,16 @@ local prettier = {
   end,
 }
 
+local black = {
+  function()
+    return {
+      exe = "black",
+      args = {"-"},
+      stdin = true
+    }
+  end,
+}
+
 local cargofmt = {
   function()
     return {
@@ -33,6 +43,7 @@ require('formatter').setup({
      typescriptreact = prettier,
      typescript = prettier,
      json = prettier,
+     python = black,
      cpp = clang,
      rust = cargofmt,
   }
