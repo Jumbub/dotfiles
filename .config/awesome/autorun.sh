8c16505f2aa7b4fe1433b90000809719f1589ace
Jamie Bray <jamie@jamiebray.xyz>
2025-03-12 15:40:07 +0800

    fresh install changes

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -2,14 +2,8 @@
 
 run="$HOME/scripts/run-if-not"
 
-# Audio manager
-$run --notify "start-pulseaudio-x11" "pulseaudio"
-
 # Blue light filter
 $run --notify "redshift"
 
 # Clipboard manager
 $run --notify "greenclip daemon" "greenclip"
-
-# Bluetooth
-$run --notify "blueman-applet"
