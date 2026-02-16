03c7de26e9d0c864d90c148ddafa6620d8eb9bde
james.bray <james.bray@eagleview.com>
2026-02-16 14:14:41 +0800

    slightly more automatic treesitter

--- .config/nvim/lua/packages/treesitter.lua
+++ .config/nvim/lua/packages/treesitter.lua
@@ -1,3 +1,11 @@
+local function has_value(tab, val)
+  for index, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+  return false
+end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -5,9 +13,10 @@ return {
     branch = "main",
     build = ":TSUpdate",
     config = function()
+      require("nvim-treesitter").install({ "gomod", "gosum", "go", "c_sharp" })
       vim.api.nvim_create_autocmd("FileType", {
         callback = function(ctx)
-          local hasStarted = pcall(vim.treesitter.start) -- errors for filetypes with no parser
+          pcall(vim.treesitter.start)
         end,
       })
     end,
