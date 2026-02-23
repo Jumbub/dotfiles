605a6b8ef0739843b83cafc5c085d683490f02a7
james.bray <james.bray@eagleview.com>
2026-02-23 17:35:26 +0800

    github link generator

--- .config/nvim/lua/packages/git.lua
+++ .config/nvim/lua/packages/git.lua
@@ -1,4 +1,21 @@
 return {
   { "tpope/vim-fugitive", cmd = "Git", lazy = true }, -- Git wrapper
   { "lewis6991/gitsigns.nvim", opts = {} }, -- Git in gutter
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    lazy = true,
+    config = function()
+      require("gitlinker").setup({
+        router = {
+          browse = {
+            ["^github%.eagleview%.com"] = require("gitlinker.routers").github_browse,
+          },
+          blame = {
+            ["^github%.eagleview%.com"] = require("gitlinker.routers").github_blame,
+          },
+        },
+      })
+    end,
+  },
 }
